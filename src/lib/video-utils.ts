22a7580b807634ef609b49955320d61a4436bd6f
oleg ivanenko <arrowplume@gmail.com>
2025-12-15 21:54:41 +0000

    опять https://pic.rutube.ru/video/6b/04/6b040e842925e81877b083d18f12790a

--- src/lib/video-utils.ts
+++ src/lib/video-utils.ts
@@ -18,6 +18,7 @@ export async function getThumbnailFromUrl(url: string): Promise<string | null> {
       const videoId = rutubeMatch[1];
       const part1 = videoId.substring(0, 2);
       const part2 = videoId.substring(2, 4);
+      // Corrected the domain to rutubelist.ru as pointed out by the user.
       return `https://pic.rutube.ru/video/${part1}/${part2}/${videoId}.jpg`;
     }
 
