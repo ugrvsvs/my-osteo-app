fdea2d80d73958f0f040e693624b25a09aad47a8
oleg ivanenko <arrowplume@gmail.com>
2025-12-15 16:47:13 +0000

    теперь на старте убери пока визуально необходимость входить . потом пере

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import { useUser } from '@/firebase';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
-  const { user, loading } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading) {
-      if (user) {
-        router.replace('/dashboard');
-      } else {
-        router.replace('/login');
-      }
-    }
-  }, [user, loading, router]);
+    router.replace('/dashboard');
+  }, [router]);
 
   return (
      <div className="flex min-h-screen items-center justify-center">
