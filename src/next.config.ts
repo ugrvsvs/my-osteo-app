539d18304dc7c984eb1fdb3257762d7010195096
oleg ivanenko <arrowplume@gmail.com>
2025-12-15 22:16:57 +0000

    так у рутуб нет первью поэтому пока убери этот код

--- src/next.config.ts
+++ src/next.config.ts
@@ -35,12 +35,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'pic.rutubelist.ru',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
